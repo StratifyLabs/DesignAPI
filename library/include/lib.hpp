fb871ef70c7d2069e55ab21b0879c69ecef23706
Tyler <tyler-gilbert@users.noreply.github.com>
2021-12-02 09:59:49 -0700

    Initial commit

---
+++ library/include/lib.hpp
@@ -0,0 +1,15 @@
+// Copyright 2016-2021 Tyler Gilbert and Stratify Labs, Inc; see LICENSE.md
+
+#ifndef JSONAPI_JSON_HPP
+#define JSONAPI_JSON_HPP
+
+namespace json {
+}
+
+#include "json/Json.hpp"
+#include "json/JsonDocument.hpp"
+#include "json/macros.hpp"
+
+using namespace json;
+
+#endif // JSONAPI_JSON_HPP
