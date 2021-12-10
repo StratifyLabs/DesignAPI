ad13ddabdfe05d35003c7bf912ebc4895aca1a8a
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-12-10 11:17:13 -0700

    Add extras subfolder for catch all

---
+++ library/include/design/extras/Cards.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tyler Gilbert on 12/10/21.
+//
+
+#include "Cards.hpp"
---
+++ library/include/design/extras/Cards.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Tyler Gilbert on 12/10/21.
+//
+
+#ifndef DESIGNLAB_CARDS_HPP
+#define DESIGNLAB_CARDS_HPP
+
+
+
+class Cards {
+
+};
+
+
+
+#endif //DESIGNLAB_CARDS_HPP
