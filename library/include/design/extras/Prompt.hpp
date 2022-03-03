62ae31feb9a6e9c4fc71503ed8bd28360e6fc655
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-03-02 19:36:51 -0700

    minor tweaks

--- library/include/design/extras/Prompt.hpp
+++ library/include/design/extras/Prompt.hpp
@@ -15,18 +15,19 @@ namespace design {
 class Prompt : public lvgl::ObjectAccess<Prompt> {
 public:
   struct Data : public lvgl::UserDataAccess<Data> {
-    API_PMAZ(accept,Data,const char *, LV_SYMBOL_OK);
-    API_PMAZ(accept_callback,Data,lvgl::Event::Callback,nullptr);
-    API_PMAZ(message,Data,const char *, "message");
-    API_PMAZ(name,Data,const char *, "");
-    API_PMAZ(reject,Data,const char *, LV_SYMBOL_CLOSE);
-    API_PMAZ(reject_callback,Data,lvgl::Event::Callback,nullptr);
-    API_PMAZ(title,Data,const char *, "title");
+    Data(const char *name = "") : lvgl::UserDataAccess<Data>(name) {}
+
+    API_PMAZ(accept, Data, const char *, LV_SYMBOL_OK);
+    API_PMAZ(accept_callback, Data, lvgl::Event::Callback, nullptr);
+    API_PMAZ(message, Data, const char *, "message");
+    API_PMAZ(name, Data, const char *, "");
+    API_PMAZ(reject, Data, const char *, LV_SYMBOL_CLOSE);
+    API_PMAZ(reject_callback, Data, lvgl::Event::Callback, nullptr);
+    API_PMAZ(title, Data, const char *, "title");
   };
 
-  Prompt(Data & data);
-  Prompt(lv_obj_t * object){ m_object = object; }
-
+  Prompt(Data &data);
+  Prompt(lv_obj_t *object) { m_object = object; }
 
 private:
   struct Names {
@@ -39,8 +40,6 @@ private:
     DESIGN_DECLARE_NAME(reject_button);
     DESIGN_DECLARE_NAME(title_label);
   };
-
-
 };
 
 } // namespace design
