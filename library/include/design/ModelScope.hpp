6e6cf65fb339086673a5433abec744f460cf923e
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-03-07 09:35:57 -0700

    Add files for ModelScope

---
+++ library/include/design/ModelScope.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Tyler Gilbert on 3/7/22.
+//
+
+#ifndef RUSHROBOTS_MODELSCOPE_HPP
+#define RUSHROBOTS_MODELSCOPE_HPP
+
+
+
+class ModelScope {
+
+};
+
+
+
+#endif //RUSHROBOTS_MODELSCOPE_HPP
---
+++ library/src/ModelScope.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tyler Gilbert on 3/7/22.
+//
+
+#include "ModelScope.hpp"
