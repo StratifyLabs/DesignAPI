bd57fcb9c1e93835148db2635c9ea5d6925deec1
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-03-07 08:32:35 -0700

    Loading circles updates

--- library/include/design/extras/LoadingCircles.hpp
+++ library/include/design/extras/LoadingCircles.hpp
@@ -5,12 +5,26 @@
 #ifndef RUSHROBOTS_LOADINGCIRCLES_HPP
 #define RUSHROBOTS_LOADINGCIRCLES_HPP
 
+#include <lvgl/ObjectAccess.hpp>
 
+#include "../macros.hpp"
 
-class LoadingCircles {
+namespace design {
+
+class LoadingCircles : public lvgl::ObjectAccess<LoadingCircles> {
+public:
+  LoadingCircles(const char * name, lv_coord_t height);
+  LVGL_OBJECT_ACCESS_CONSTRUCT_FROM_OBJECT(LoadingCircles);
+  LoadingCircles& update_progress(int value);
+
+private:
+  struct Names {
+    DESIGN_DECLARE_NAME(circle_row);
+  };
+
+  void update_size(int offset);
 
 };
 
-
-
-#endif //RUSHROBOTS_LOADINGCIRCLES_HPP
+}
+#endif // RUSHROBOTS_LOADINGCIRCLES_HPP
--- library/src/extras/LoadingCircles.cpp
+++ library/src/extras/LoadingCircles.cpp
@@ -2,4 +2,72 @@
 // Created by Tyler Gilbert on 2/23/22.
 //
 
-#include "LoadingCircles.hpp"
+#include <lvgl/Label.hpp>
+
+#include "design/Grid.hpp"
+
+#include "design/extras/LoadingCircles.hpp"
+
+using namespace lvgl;
+using namespace design;
+
+LoadingCircles::LoadingCircles(const char *name, lv_coord_t height) {
+  construct_object(name);
+
+  set_height(height);
+  const auto icon_height = height * 9 / 10;
+  const auto active_size = Size(icon_height, icon_height);
+  const auto inactive_size = Size(icon_height * 2 / 3, icon_height * 2 / 3);
+  static const auto active_style
+    = Style().set_height(icon_height).set_width(icon_height);
+
+  add(Row(Names::circle_row)
+        .center()
+        .justify_space_between()
+        .fill_height()
+        .set_width(icon_height * 4)
+        .add(NakedContainer()
+               .set_size(active_size)
+               .add(Label()
+                      .center()
+                      .add_style("card")
+                      .add_style(active_style, State::user1)
+                      .set_radius(LV_RADIUS_CIRCLE)
+                      .set_size(inactive_size)))
+        .add(NakedContainer()
+               .set_size(active_size)
+               .add(Label()
+                      .center()
+                      .add_style("card")
+                      .add_style(active_style, State::user1)
+                      .set_radius(LV_RADIUS_CIRCLE)
+                      .set_size(inactive_size)))
+        .add(NakedContainer()
+               .set_size(active_size)
+               .add(Label()
+                      .center()
+                      .add_style("card")
+                      .add_style(active_style, State::user1)
+                      .set_radius(LV_RADIUS_CIRCLE)
+                      .set_size(inactive_size)))
+
+  );
+}
+
+LoadingCircles &LoadingCircles::update_progress(int value) {
+  int offset = value % 3;
+  update_size(offset);
+  return *this;
+}
+
+void LoadingCircles::update_size(int offset) {
+  auto row = find<Row>(Names::circle_row);
+  for (auto i : api::Index(3)) {
+    auto label = row.get_child(i).get_child<Label>(0);
+    if (i == offset) {
+      label.add_state(State::user1);
+    } else {
+      label.clear_state(State::user1);
+    }
+  }
+}
