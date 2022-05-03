4c7e6be40260c16357a2362cc060a8d25de451b6
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-05-02 18:07:46 -0600

    Add design::Plot

--- library/include/design/Plot.hpp
+++ library/include/design/Plot.hpp
@@ -5,6 +5,7 @@
 #ifndef DESIGN_API_DESIGN_PLOT_HPP
 #define DESIGN_API_DESIGN_PLOT_HPP
 
+#include <fs/FileObject.hpp>
 #include <lvgl/Chart.hpp>
 #include <lvgl/Label.hpp>
 
@@ -16,38 +17,71 @@ class Draw;
 
 namespace design {
 
-
-class Legend : public lvgl::ObjectAccess<Legend> {
-public:
-
-  class Series : public lvgl::ObjectAccess<Series> {
-  public:
-    LVGL_OBJECT_ACCESS_DECLARE_CONSTRUCTOR(Series);
-
-    Series & set_marker_color(lvgl::Color color);
-    Series & set_label(const char * value);
-    Series & set_label_as_static(const char * value);
-
-  private:
-    struct Names {
-      DESIGN_DECLARE_NAME(marker);
-      DESIGN_DECLARE_NAME(label);
-    };
-  };
-
-
-  LVGL_OBJECT_ACCESS_DECLARE_CONSTRUCTOR(Legend);
-};
-
 class Plot : public lvgl::ObjectAccess<Plot> {
 public:
   struct Data;
-  using FormatLabelCallback = void (*)(Data * data, lvgl::Chart::Axis axis, lv_coord_t value, var::View text);
+
+  class Legend : public lvgl::ObjectAccess<Legend> {
+  public:
+    class Series : public lvgl::ObjectAccess<Series> {
+    public:
+      LVGL_OBJECT_ACCESS_DECLARE_CONSTRUCTOR(Series);
+
+      Series &set_marker_color(lvgl::Color color);
+      Series &set_label(const char *value);
+      Series &set_label_as_static(const char *value);
+
+    private:
+      struct Names {
+        DESIGN_DECLARE_NAME(marker);
+        DESIGN_DECLARE_NAME(label);
+      };
+    };
+
+    LVGL_OBJECT_ACCESS_DECLARE_CONSTRUCTOR(Legend);
+  };
+
+  using FormatLabelCallback =
+    void (*)(Data *data, lvgl::Chart::Axis axis, lv_coord_t value, var::View text);
+
+
+  static constexpr ssize_t get_next_data = -1;
+  static constexpr ssize_t get_data_count = -2;
+
+  using GetDataCallback = lv_coord_t (
+      *)(Plot * plot, lvgl::Chart::Axis axis, size_t series_number, ssize_t point);
 
   struct Data : lvgl::UserDataAccess<Data> {
-    Data(const char *name) : UserDataBase(name) {}
+    explicit Data(const char *name) : UserDataBase(name) {}
 
+    API_PMAZ(associated_object, Data, lv_obj_t *, nullptr);
+    API_PMAZ(count, Data, size_t, {});
     API_PMAZ(format_label, Data, FormatLabelCallback, nullptr);
+    API_PMAZ(get_data, Data, GetDataCallback, nullptr);
+    API_PMAZ(offset, Data, size_t, {});
+
+  };
+
+  class WindowSlider : public lvgl::ObjectAccess<WindowSlider> {
+  public:
+    LVGL_OBJECT_ACCESS_DECLARE_CONSTRUCTOR(WindowSlider);
+
+  private:
+    friend Plot;
+    struct Names {
+      DESIGN_DECLARE_NAME(plot_control_window_slider);
+    };
+
+    struct OffsetCount {
+      size_t offset;
+      size_t count;
+    };
+
+
+    WindowSlider& set_value(size_t offset, size_t count, size_t total);
+    OffsetCount get_offset_and_count(size_t total) const;
+
+    static void value_changed(lv_event_t *e);
   };
 
   LVGL_OBJECT_ACCESS_DECLARE_CONSTRUCTOR(Plot);
@@ -64,6 +98,10 @@ public:
     lvgl::Alignment alignement = lvgl::Alignment::center);
   Plot &set_horizontal_label_margin_percentage(lv_coord_t value);
   Plot &set_vertical_label_margin_percentage(lv_coord_t value);
+
+  Plot &add_control(const lvgl::Object &object);
+  Plot &add_legend(const Legend &legend);
+  Plot &set_window(size_t offset, size_t count, size_t total);
 
   lvgl::Label get_title() const { return find<lvgl::Label>(Names::title); }
 
@@ -85,10 +123,26 @@ public:
 
   lvgl::Chart get_chart() const { return find<lvgl::Chart>(Names::plot_chart); }
 
+  template<class Associated> Associated get_associated() const {
+    return Associated(data()->associated_object);
+  }
+
+  size_t offset() const {
+    return data()->offset;
+  }
+
+  size_t count() const {
+    return data()->count;
+  }
+
+  bool is_right_justified() const;
+  bool is_left_justified() const;
+
 private:
   struct Names {
     DESIGN_DECLARE_NAME(title);
     DESIGN_DECLARE_NAME(container);
+    DESIGN_DECLARE_NAME(main_column);
     DESIGN_DECLARE_NAME(row_for_chart);
     DESIGN_DECLARE_NAME(plot_chart);
     DESIGN_DECLARE_NAME(primary_x_axis_label);
@@ -100,8 +154,13 @@ private:
 
   static void handle_draw_part_begin(lv_event_t *e);
 
+  void load_data(size_t point_offset, size_t point_count);
+  size_t get_maximum_point_count(size_t offset);
+
   Data *data() { return user_data<Data>(); }
   const Data *data() const { return user_data<Data>(); }
+
+  static Plot get_plot_from_child_event(lv_event_t *e);
 };
 
 } // namespace design
--- library/src/Plot.cpp
+++ library/src/Plot.cpp
@@ -3,6 +3,8 @@
 //
 #include <lvgl/Draw.hpp>
 #include <lvgl/Event.hpp>
+#include <lvgl/Generic.hpp>
+#include <lvgl/Slider.hpp>
 
 #include "design/Grid.hpp"
 #include "design/Plot.hpp"
@@ -10,8 +12,9 @@
 
 using namespace lvgl;
 using namespace design;
+using namespace var;
 
-Legend::Legend(const char *name) {
+Plot::Legend::Legend(const char *name) {
   construct_object(name);
   add_style("card");
   set_height(size_from_content);
@@ -21,7 +24,7 @@ Legend::Legend(const char *name) {
   set_row_padding(15);
 }
 
-Legend::Series::Series(const char *name) {
+Plot::Legend::Series::Series(const char *name) {
   construct_object(name);
   add_style(Row::get_style());
   set_height(size_from_content);
@@ -37,49 +40,49 @@ Legend::Series::Series(const char *name) {
   add(Label(Names::label).add_style("plot_legend_label").set_text_as_static(""));
 }
 
-Legend::Series &Legend::Series::set_marker_color(lvgl::Color color) {
+Plot::Legend::Series &Plot::Legend::Series::set_marker_color(lvgl::Color color) {
   find<Container>(Names::marker).set_background_color(color);
   return *this;
 }
 
-Legend::Series &Legend::Series::set_label_as_static(const char *value) {
+Plot::Legend::Series &Plot::Legend::Series::set_label_as_static(const char *value) {
   find<Label>(Names::label).set_text_as_static(value);
   return *this;
 }
 
-Legend::Series &Legend::Series::set_label(const char *value) {
+Plot::Legend::Series &Plot::Legend::Series::set_label(const char *value) {
   find<Label>(Names::label).set_text(value);
   return *this;
 }
 
 Plot::Plot(const char *name) {
   construct_object(name);
-
-  add_style(Column::get_style()).add_style("col");
-  add(Heading4(Names::title, "Plot Title"));
-  add(Row(Names::row_for_chart)
-        .fill_width()
-        .set_flex_grow()
-        .add(Label(Names::primary_y_axis_label)
-               .set_text_as_static("Y")
-               .set_width(0)
-               .add_flag(Flags::hidden))
-        .add(NakedContainer(Names::container)
+  add(Column(Names::main_column).fill());
+  auto column = find<Column>(Names::main_column);
+  column.add(Heading4(Names::title, "Plot Title"));
+  column.add(Row(Names::row_for_chart)
+               .fill_width()
                .set_flex_grow()
-               .fill_height()
-               .add(Chart(Names::plot_chart)
-                      .set_width(95_percent)
-                      .set_height(80_percent)
-                      .center()
-                      .set_clip_corner(false)
-                      .add_event_callback(
-                        EventCode::draw_part_begin, handle_draw_part_begin))));
+               .add(Label(Names::primary_y_axis_label)
+                      .set_text_as_static("Y")
+                      .set_width(0)
+                      .add_flag(Flags::hidden))
+               .add(NakedContainer(Names::container)
+                      .set_flex_grow()
+                      .fill_height()
+                      .add(Chart(Names::plot_chart)
+                             .set_width(95_percent)
+                             .set_height(80_percent)
+                             .center()
+                             .set_clip_corner(false)
+                             .add_event_callback(
+                               EventCode::draw_part_begin, handle_draw_part_begin))));
 
-  add(Label(Names::primary_x_axis_label)
-        .set_text_as_static("X")
-        .add_style("plot_label")
-        .add_flag(Flags::hidden)
-        .set_height(0));
+  column.add(Label(Names::primary_x_axis_label)
+               .set_text_as_static("X")
+               .add_style("plot_label")
+               .add_flag(Flags::hidden)
+               .set_height(0));
 }
 
 Plot &Plot::set_title(const char *title) {
@@ -140,6 +143,16 @@ Plot &Plot::set_secondary_y_axis_label(const char *value) {
   return *this;
 }
 
+Plot &Plot::add_control(const Object &object) {
+  find<Column>(Names::main_column).add(lvgl::Generic(object.object()));
+  return *this;
+}
+
+Plot &Plot::add_legend(const Legend &legend) {
+  find<Column>(Names::main_column).add(Legend(legend.object()));
+  return *this;
+}
+
 void Plot::handle_draw_part_begin(lv_event_t *e) {
   auto event = Event(e);
   auto chart = event.target<Chart>();
@@ -166,15 +179,191 @@ void Plot::handle_draw_part_begin(lv_event_t *e) {
     }
   }
 }
+Plot Plot::get_plot_from_child_event(lv_event_t *e) {
+  return Event(e).target().find_parent<Object>(Names::main_column).get_parent<Plot>();
+}
 
-static void draw_event_cb(lv_event_t *e) {
-  lv_obj_draw_part_dsc_t *dsc = lv_event_get_draw_part_dsc(e);
-  if (!lv_obj_draw_part_check_type(dsc, &lv_chart_class, LV_CHART_DRAW_PART_TICK_LABEL))
-    return;
+size_t Plot::get_maximum_point_count(size_t offset) {
+  size_t result = SIZE_T_MAX;
+  size_t series_index = 0;
 
-  if (dsc->id == LV_CHART_AXIS_PRIMARY_X && dsc->text) {
-    const char *month[] = {"Jan",  "Febr", "March", "Apr", "May", "Jun",
-                           "July", "Aug",  "Sept",  "Oct", "Nov", "Dec"};
-    lv_snprintf(dsc->text, dsc->text_length, "%s", month[dsc->value]);
+  auto update_result = [&](Chart::Axis axis) {
+    if (const auto count = data()->get_data(this, axis, series_index, get_data_count);
+        count < result) {
+      result = count;
+    }
+  };
+
+  auto series = get_chart().get_series();
+  while (series.is_valid()) {
+    update_result(Chart::Axis::primary_x);
+    update_result(Chart::Axis::primary_y);
+    update_result(Chart::Axis::secondary_x);
+    update_result(Chart::Axis::secondary_y);
+    ++series_index;
+    series = series.get_next_series();
   }
+
+  if (result > offset) {
+    return result - offset;
+  }
+  return 0;
+}
+
+Plot &Plot::set_window(size_t offset, size_t count, size_t total) {
+  load_data(offset, count);
+  // synchronize the window slider with these values
+  auto slider =
+    find<Slider, IsAssertOnFail::no>(WindowSlider::Names::plot_control_window_slider);
+
+  if (slider.is_valid()) {
+    API_PRINTF_TRACE_LINE();
+    slider.get_parent<WindowSlider>().set_value(data()->offset, data()->count, total);
+    API_PRINTF_TRACE_LINE();
+  }
+
+  return *this;
+}
+
+void Plot::load_data(size_t point_offset, size_t point_count) {
+
+  auto chart = get_chart();
+  const auto effective_point_count = [&]() {
+    const auto maximum_point_count = get_maximum_point_count(point_offset);
+    return point_count > maximum_point_count ? maximum_point_count : point_count;
+  }();
+  printf("count given:%d effective:%d\n", point_count, effective_point_count);
+  chart.set_point_count(effective_point_count);
+
+  data()->offset = point_offset;
+  data()->count = effective_point_count;
+
+  auto primary_x = Range().set_maximum(-INT16_MAX).set_minimum(INT16_MAX);
+  auto primary_y = primary_x;
+  auto secondary_x = primary_x;
+  auto secondary_y = primary_x;
+
+  const auto is_scatter = chart.get_type() == Chart::Type::scatter;
+
+  auto series = chart.get_series();
+  size_t series_index = 0;
+  while (series.is_valid()) {
+    for (auto i : api::Index(effective_point_count)) {
+      const auto point_index = i ? get_next_data : point_offset;
+      const auto x_value = [&]() {
+        if (is_scatter) {
+          const auto x_axis = series.is_x_axis_secondary() ? Chart::Axis::secondary_x
+                                                           : Chart::Axis::primary_x;
+
+          return data()->get_data(this, x_axis, series_index, point_index);
+        }
+        return lv_coord_t{};
+      }();
+
+      if (series.is_x_axis_secondary()) {
+        secondary_x.expand_to_include(x_value);
+      } else {
+        primary_x.expand_to_include(x_value);
+      }
+
+      const auto y_axis =
+        series.is_y_axis_secondary() ? Chart::Axis::secondary_y : Chart::Axis::primary_y;
+
+      auto y_value = data()->get_data(this, y_axis, series_index, point_index);
+
+      if (series.is_y_axis_secondary()) {
+        secondary_y.expand_to_include(y_value);
+      } else {
+        primary_y.expand_to_include(y_value);
+      }
+
+      if (is_scatter) {
+        series.set_value_by_id(i, Point{x_value, y_value});
+
+      } else {
+        series.set_value_by_id(i, y_value);
+      }
+    }
+    series = series.get_next_series();
+    ++series_index;
+  }
+
+  if (effective_point_count > 0) {
+    if (is_scatter) {
+      chart.set_range(Chart::Axis::primary_x, primary_x)
+        .set_range(Chart::Axis::secondary_x, secondary_x);
+    }
+
+    chart.set_range(Chart::Axis::primary_y, primary_y)
+      .set_range(Chart::Axis::secondary_y, secondary_y);
+  }
+}
+
+bool Plot::is_right_justified() const {
+  auto slider =
+    find<Slider, IsAssertOnFail::no>(WindowSlider::Names::plot_control_window_slider);
+  if (slider.is_valid()) {
+    return slider.get_value() == slider.get_range().maximum();
+  }
+  return false;
+}
+
+bool Plot::is_left_justified() const {
+  auto slider =
+    find<Slider, IsAssertOnFail::no>(WindowSlider::Names::plot_control_window_slider);
+  if (slider.is_valid()) {
+    return slider.get_left_value() == 0;
+  }
+  return false;
+}
+
+Plot::WindowSlider::WindowSlider(const char *name) {
+  construct_object(name);
+  set_background_opacity(Opacity::transparent);
+  add(Slider(Names::plot_control_window_slider)
+        .center()
+        .set_mode(Slider::Mode::range)
+        .set_range(Range())
+        .set_left_value(0)
+        .set_value(100)
+        .set_width(90_percent)
+        .add_event_callback(EventCode::value_changed, value_changed));
+}
+
+void Plot::WindowSlider::value_changed(lv_event_t *e) {
+  // all slider events
+  auto plot = get_plot_from_child_event(e);
+  if (plot.data()->get_data) {
+    auto slider = Event(e).target<Slider>();
+    const auto total_count = plot.get_maximum_point_count(0);
+    const auto left_value = slider.get_left_value();
+    const auto right_value = slider.get_value();
+    const auto offset = left_value * total_count / slider.get_range().maximum();
+    const auto count = right_value * total_count / slider.get_range().maximum() - offset;
+    plot.load_data(offset, count);
+  }
+}
+
+Plot::WindowSlider &
+Plot::WindowSlider::set_value(size_t offset, size_t count, size_t total) {
+  auto slider = find<Slider>(Names::plot_control_window_slider);
+  const auto range_maximum = slider.get_range().maximum();
+  const auto left_value = offset * range_maximum / total;
+  const auto right_value = left_value + count * range_maximum / total;
+  const auto effective_right_value =
+    offset + count == total ? range_maximum : right_value;
+
+  slider.set_value(effective_right_value).set_left_value(left_value);
+  return *this;
+}
+
+Plot::WindowSlider::OffsetCount
+Plot::WindowSlider::get_offset_and_count(size_t total) const {
+  auto slider = find<Slider>(Names::plot_control_window_slider);
+  const auto left_value = slider.get_left_value();
+  const auto right_value = slider.get_value();
+  const auto offset_value = left_value * total / slider.get_range().maximum();
+  return {
+    .offset = offset_value,
+    .count = right_value * total / slider.get_range().maximum() - offset_value};
 }
