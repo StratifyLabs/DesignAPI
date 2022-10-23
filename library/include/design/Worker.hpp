c0d91f2dfdf08f6e8dea0483e98b1497879df536
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-23 13:36:38 -0500

    Update Worker using api::Function

--- library/include/design/Worker.hpp
+++ library/include/design/Worker.hpp
@@ -18,80 +18,58 @@ namespace design {
 
 class Worker : public api::ExecutionContext {
 public:
-  using Work = void (*)(Worker *);
+  using Task = api::Function<void()>;
   Worker() : m_cond(m_mutex) {}
-  Worker(lvgl::Runtime *runtime, Work work);
 
-  Worker(const Worker &) = delete;
-  Worker &operator=(const Worker &) = delete;
-
-  Worker(Worker &&a) : m_cond(m_mutex) { move_worker(a); }
-
+  Worker(Worker &&a) : m_cond(m_mutex) { swap_worker(a); }
   Worker &operator=(Worker &&a) {
-    move_worker(a);
+    swap_worker(a);
     return *this;
   }
 
   lvgl::Runtime &runtime();
   const lvgl::Runtime &runtime() const;
   bool is_running() const;
-  Worker &start(const thread::Thread::Attributes &thread_attributes = {});
   Worker &wait_runtime_task();
+  void start(const thread::Thread::Attributes &thread_attributes = {});
 
 protected:
-  void push_task_void_to_runtime(void *user_data, void (*task)(void *));
-  static void notify_task(lv_obj_t *object);
+  Worker(lvgl::Runtime *runtime, lv_obj_t *associated_object);
 
-  void set_associated_object(lv_obj_t *object) { m_associated_object = object; }
+  void push_user_task_to_runtime(Task task);
+  static void notify_task(lv_obj_t *object);
 
 private:
   lvgl::Runtime *m_runtime = nullptr;
   thread::Mutex m_mutex;
   thread::Cond m_cond;
-  void *m_user_data = nullptr;
-  void (*m_user_task)(void *) = nullptr;
-  Work m_work_callback = nullptr;
+  Task m_user_task;
   thread::Thread m_thread;
   API_RAF(Worker, lv_obj_t *, associated_object, nullptr);
 
-  void move_worker(Worker &a);
-  static void *thread_work_function(void *args);
-  void work_function();
-  void lock_user_data(void *user_data, void (*task)(void *));
-  void unlock_user_data();
-  static void execute_runtime_task_function(void *context);
+  void swap_worker(Worker &a);
+  void lock_user_task(Task task);
+  void unlock_user_task();
   void execute_runtime_task();
+
+  virtual void work() = 0;
 };
 
 template <class Derived> class WorkerAccess : public Worker {
-  using VoidTask = void (*)(void *);
-
 public:
   WorkerAccess() = default;
-  WorkerAccess(lvgl::Runtime *runtime)
-    : Worker(runtime, work_callback_function) {}
+  WorkerAccess(
+    lvgl::Runtime *runtime,
+    lv_obj_t *associated_object)
+    : Worker(runtime, associated_object) {}
 
-  Derived &set_associated_object(lv_obj_t *object) {
-    Worker::set_associated_object(object);
-    return static_cast<Derived &>(*this);
-  }
-
-  template <class ArgumentType>
-  Derived &
-  push_task_to_runtime(ArgumentType *user_data, void (*task)(ArgumentType *)) {
-    push_task_void_to_runtime(
-      (void *)user_data,
-      reinterpret_cast<VoidTask>(task));
-    return static_cast<Derived &>(*this);
-  }
-
-  Derived &push_task_to_runtime(void (*task)(void *)) {
-    push_task_void_to_runtime(nullptr, task);
+  Derived &push_task_to_runtime(Task task) {
+    push_user_task_to_runtime(std::move(task));
     return static_cast<Derived &>(*this);
   }
 
   Derived &notify(lv_obj_t *object) {
-    push_task_to_runtime<lv_obj_t>(object, notify_task);
+    push_user_task_to_runtime([object]() { notify_task(object); });
     return static_cast<Derived &>(*this);
   }
 
@@ -102,18 +80,8 @@ public:
     return static_cast<Derived &>(*this);
   }
 
-
-protected:
-  static void work_callback_function(Worker *worker) {
-    static_cast<WorkerAccess<Derived> *>(worker)->work();
-  }
-
-  virtual void work() = 0;
-
 private:
-
 };
-
 
 } // namespace design
 
--- library/src/Worker.cpp
+++ library/src/Worker.cpp
@@ -2,8 +2,8 @@
 // Created by Tyler Gilbert on 12/16/21.
 //
 
-#include <lvgl/Generic.hpp>
 #include <lvgl/Event.hpp>
+#include <lvgl/Generic.hpp>
 
 #if !defined LVGL_RUNTIME_TASK_ARRAY_SIZE
 #error "DesignAPI requires LVGL_RUNTIME_TASK_ARRAY_SIZE to be defined in lv_conf.h"
@@ -14,82 +14,65 @@
 using namespace design;
 using namespace lvgl;
 
-Worker::Worker(lvgl::Runtime *runtime, Worker::Work work)
-  : m_runtime(runtime), m_cond(m_mutex), m_work_callback(work) {}
+Worker::Worker(lvgl::Runtime *runtime, lv_obj_t *associated_object)
+  : m_runtime(runtime), m_cond(m_mutex), m_associated_object{associated_object} {}
 
-void *Worker::thread_work_function(void *args) {
-  reinterpret_cast<Worker *>(args)->work_function();
-  return nullptr;
-}
-
-Worker &Worker::start(const thread::Thread::Attributes &thread_attributes) {
+void Worker::start(const thread::Thread::Attributes &thread_attributes) {
   API_ASSERT(m_runtime != nullptr);
   if (!is_running()) {
-    m_thread = thread::Thread(thread_attributes, this, thread_work_function);
+    m_thread = thread::Thread(thread_attributes, [this]() -> void * {
+      work();
+      return nullptr;
+    });
   }
-  return *this;
 }
 
-void Worker::move_worker(Worker &a) {
+void Worker::swap_worker(Worker &a) {
   // Cannot move a running thread
   API_ASSERT(a.is_running() == false);
-
   std::swap(m_runtime, a.m_runtime);
-  std::swap(m_user_data, a.m_user_data);
   std::swap(m_user_task, a.m_user_task);
-  std::swap(m_work_callback, a.m_work_callback);
   std::swap(m_associated_object, a.m_associated_object);
 }
 
-void Worker::work_function() {
-  API_ASSERT(m_work_callback != nullptr);
-  m_work_callback(this);
-}
-
-void Worker::lock_user_data(void *user_data, void (*task)(void *)) {
+void Worker::lock_user_task(Task task) {
   // this is run in the worker thread
   thread::Mutex::Scope ms(m_cond.mutex());
-  while (m_user_task != nullptr) {
+  while (m_user_task) {
     m_cond.wait();
   }
   m_user_task = task;
-  m_user_data = user_data;
 }
 
-void Worker::unlock_user_data() {
+void Worker::unlock_user_task() {
   thread::Mutex::Scope ms(m_cond.mutex());
   api::ErrorScope error_scope;
-  m_user_task = nullptr;
-  m_user_data = nullptr;
+  m_user_task = {};
   m_cond.signal();
 }
 
-Worker &Worker::wait_runtime_task(){
-  lock_user_data(nullptr, nullptr);
+Worker &Worker::wait_runtime_task() {
+  lock_user_task(nullptr);
   return *this;
 }
 
-void Worker::push_task_void_to_runtime(void *user_data, void (*task)(void *)) {
-  lock_user_data(user_data, task);
-  runtime().push(this, execute_runtime_task_function);
-}
-
-void Worker::execute_runtime_task_function(void *context) {
-  reinterpret_cast<Worker *>(context)->execute_runtime_task();
+void Worker::push_user_task_to_runtime(Task task) {
+  lock_user_task(std::move(task));
+  runtime().push([this]() { execute_runtime_task(); });
 }
 
 void Worker::execute_runtime_task() {
-  m_user_task(m_user_data);
-  unlock_user_data();
+  if (m_user_task) {
+    m_user_task();
+  }
+  unlock_user_task();
 }
 
-void Worker::notify_task(lv_obj_t * object) {
+void Worker::notify_task(lv_obj_t *object) {
   Event::send(Generic(object), EventCode::notified);
 }
 
-bool Worker::is_running() const {
-  return m_thread.is_valid() && m_thread.is_running();
-}
+bool Worker::is_running() const { return m_thread.is_valid() && m_thread.is_running(); }
 
 lvgl::Runtime &Worker::runtime() {
   API_ASSERT(m_runtime != nullptr);
